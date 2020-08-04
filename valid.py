a70528433aa528802edb89416973b329c5b8785a
tankchiu <42413252+s107321509@users.noreply.github.com>
2020-08-04 13:13:24 +0800

    Update valid.py

--- valid.py
+++ valid.py
@@ -14,19 +14,19 @@ os.chdir(caffe_root)
 caffe.set_mode_gpu()
 caffe.set_device(0)
 
-net_file='./mvccn_12view_deploy.prototxt'  
-caffe_model='./mvcnn_iter_10000.caffemodel'
+net_file='/home/viplab/桌面/MVCNN_Caffe_12views-master/mvcnn_12view_deploy.prototxt'  
+caffe_model='/home/viplab/桌面/MVCNN_Caffe_12views-master/mvcnn_iter_10000.caffemodel'
 #mean_file='./ilsvrc_2012_mean.npy' 
 
 net = caffe.Net(net_file,caffe_model,caffe.TEST)  
-mean_file = np.load('ilsvrc_2012_mean.npy')
+mean_file = np.load('/home/viplab/桌面/MVCNN_Caffe_12views-master/ilsvrc_2012_mean.npy')
 meanPixel = mean_file.mean(1).mean(1)
 viewSize = 12
 imh = 224
 imw = 224
 imc = 3
 
-image_dir = r"/home/viplab/MVCNN/MVCNN_12views/valid/valid_al" 
+image_dir = r"home/viplab/桌面/MVCNN_Caffe_12views-master/valid/valid_al" 
 file_glob = os.path.join(image_dir,"*")
 file_list = []
 file_list.extend(glob.glob(file_glob))
@@ -43,7 +43,7 @@ ims = ims.transpose(3,2,0,1)
 net.blobs['data'].data[...] = ims
 out = net.forward()
 
-imagenet_labels_filename = './list.txt' 
+imagenet_labels_filename = '/home/viplab/桌面/MVCNN_Caffe_12views-master/list.txt' 
 labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')  
 
 top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1] 
